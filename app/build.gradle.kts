902009812e6322dffe051e0e655d8fb8a102726a
tiendung004 <dungptph36187@fpt.edu.vn>
2023-11-30 13:47:52 +0700

    update firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,10 +38,11 @@ dependencies {
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     // Import the Firebase BoM
-    implementation ( platform ( "com.google.firebase:firebase-bom:32.6.0" ))
+    implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
     implementation ("com.google.firebase:firebase-database")
     implementation("com.google.firebase:firebase-auth:22.3.0")
     implementation("com.google.firebase:firebase-database:20.3.0")
+    implementation("com.google.firebase:firebase-analytics")
     implementation("com.squareup.picasso:picasso:2.8")
 
     testImplementation("junit:junit:4.13.2")
